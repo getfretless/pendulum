8363fc0a865c30e50ac544c79b556d13ba9de320
Dave Strus <dave@davestrus.com>
2014-12-14 17:07:09 -0500

    Update variables via a form.

--- js/pendulum.js
+++ js/pendulum.js
@@ -1,33 +1,42 @@
 function canvas() {
   var context = myCanvas.getContext('2d');
-  var x = 0, y = 0, width = myCanvas.width, height = myCanvas.height;
+  var x = 0, y = 0;
   var interval;
+  var n = 20;
+  var width = 5;
+  var height = 5;
+  var multiplier = 20;
+  var timeModifier = 0.02;
 
   function stop() {
     clearInterval(interval);
   }
 
   function drawManyRectangles() {
-    var n = 100;
-    width = 2;
-    height = 2;
-    var time = 0;
+    var time = 0
     var redraw = function () {
       context.clearRect(0, 0, myCanvas.width, myCanvas.height);
-      for (var i=0; i < n; i++) { // while (i < n) {
-        x = myCanvas.width / 2 + Math.sin(time * i) *20;
+      for (var i=0; i < n; i++) {
+        x = myCanvas.width / 2 + Math.sin(time * i) * multiplier;
         y = i/n * myCanvas.height;
-        // var sin = Math.sin(i / 20 * time) / 2 + 0.5;
-        // var cos = Math.cos(i / 20 * time) /2 + 0.5;
-        // x = sin * (myCanvas.width - width);
-        // y = cos * (myCanvas.height - height);
         context.fillRect(x, y, width, height);
-        //i++; // same as `i = i+1` and `i += 1`
       }
-      time += 0.02;
+      time += timeModifier;
     }
     interval = setInterval(redraw, 20);
   }
+
+  function update(ev) {
+    ev.preventDefault();
+    var fields = this.elements;
+    n = fields.n.value;
+    width = fields.width.value;
+    height = fields.height.value;
+    multiplier = fields.multiplier.value;
+    timeModifier = parseFloat(fields.time_modifier.value) / 100;
+  }
+
+  document.querySelector('form').onsubmit = update;
   drawManyRectangles();
 }
 canvas();
